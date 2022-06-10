d1b6905ca039c502d3fd86bb2c337c25f9ee7c36
OnasChaturvedi <106997229+OnasChaturvedi@users.noreply.github.com>
2022-06-10 21:16:57 +0530

    Add C++ solutions

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -10,35 +10,43 @@ int main()
 	
 	cout << "Input a number : ";
 	cin >> num1;
-	
+		
 	cout << "Input another number : ";
 	cin >> num2;
-	
-	cout << "Press + to add, - to substract, * to mutiply and / to divide\n";
-	cin >> arfunction;
-	
-	switch (arfunction)
-	{
-		case '+': 
-			cout << num1 + num2;
-			break;
-			
-		case '-':
-			cout << num1 - num2;
-			break;
-			
-		case '*':
-			cout << num1 * num2;
-			break;
 		
-		case '/':
-			cout << num1 / num2;
-			break;
+	int n=1;	
+	while(n==1) {
+		cout << "Press + to add, - to substract, * to mutiply and / to divide\n";
+		cin >> arfunction;
 		
-		default :
-			cout << "Invalid operator\n";
-			break;
+		switch (arfunction)
+		{
+			case '+': 
+				cout << num1 + num2;
+				n=0;
+				break;
+				
+			case '-':
+				cout << num1 - num2;
+				n=0;
+				break;
+				
+			case '*':
+				cout << num1 * num2;
+				n=0;
+				break;
+			
+			case '/':
+				cout << num1 / num2;
+				n=0;
+				break;
+			
+			default :
+				cout << "Invalid operator\n";
+				break;
+		}
+		
 	}
-	
+		
 	return 0;
 }
